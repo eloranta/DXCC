0d44348ee193e63ce609608daa12fd4961448a4b
eloranta <hannu.eloranta@gmail.com>
2024-01-28 13:25:35 +0200

    add open database

--- DXCC/main.cpp
+++ DXCC/main.cpp
@@ -1,10 +1,23 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QtSql>
+
+extern bool openDatabase(const QString& name)
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(name);
+    return db.open();
+}
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    if (!openDatabase("dxcc.sqlite"))
+    {
+        qDebug() << "Cannot open database";
+        return -1;
+    }
     MainWindow w;
     w.show();
     return a.exec();
