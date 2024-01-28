b4651945a24f9e22c72e630982c37e13320bd291
eloranta <hannu.eloranta@gmail.com>
2024-01-28 14:00:42 +0200

    add create dxcc table

--- DXCC/mainwindow.cpp
+++ DXCC/mainwindow.cpp
@@ -8,6 +8,31 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     model.setTable("dxcc");
     ui->tableView->setModel(&model);
+
+    QSqlQuery query;
+    query.exec(QString("create table if not exists dxcc ("
+               "Dxcc integer primary key,"
+               "Prefic text,"
+               "Entity text,"
+               "Deleted text,"
+               "Mix text,"
+               "Ph text,"
+               "CW text,"
+               "RT text,"
+               "SAT text,"
+               "160 text,"
+               "80 text,"
+               "40 text,"
+               "30 text,"
+               "20 text,"
+               "17 text,"
+               "15 text,"
+               "12 text,"
+               "10 text,"
+               "6 text,"
+               "2 text,"
+               "70cm text,"
+               "Comment text)"));
 }
 
 MainWindow::~MainWindow()
